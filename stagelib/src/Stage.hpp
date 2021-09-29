59e4369833ffbcad48051a5c4cb7c145325a5a7f
Alistair Buxton <a.j.buxton@gmail.com>
2021-09-29 23:32:26 +0100

    Add an all-time counter to Stage
    Keeps track of the total number of stages used across the entire
    run of the program.

--- stagelib/src/Stage.hpp
+++ stagelib/src/Stage.hpp
@@ -12,10 +12,11 @@ typedef std::shared_ptr<Stage> StagePtr;
 
 class Stage : public std::enable_shared_from_this<Stage> {
     public:
-        Stage() { _count++; }
+        Stage() { _count++; _all_time_count++;}
         virtual ~Stage() { _count--; }
 
         static int count() { return _count; }
+        static int all_time_count() { return _all_time_count; }
 
         virtual void update(uint32_t, ButtonState) {}
         virtual void render() {}
@@ -33,6 +34,7 @@ class Stage : public std::enable_shared_from_this<Stage> {
         bool _changed = false;
         StagePtr _next = nullptr;
         inline static int _count = 0;
+        inline static int _all_time_count = 0;
 };
 
 
